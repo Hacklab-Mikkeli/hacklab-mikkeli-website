dc78cce13dc6ed6ba645764019d07a5143ddccee
Heikki Kurhinen <heikki.kurhinen@otavanopisto.fi>
2015-01-28 09:49:29 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+﻿
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+
+// all environments
+app.set('port', process.env.PORT || 9006);
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
